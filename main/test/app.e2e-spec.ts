1fa5830d747cb97700ce6736f17db086149f4834
umitkarasogut <umtkrsgt@hotmail.com>
2022-08-31 23:57:00 +0300

    [test] add e2e product ms test to main service

--- main/test/app.e2e-spec.ts
+++ main/test/app.e2e-spec.ts
@@ -3,8 +3,9 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('ProductServiceIntegration (e2e)', () => {
   let app: INestApplication;
+  let exampleProduct;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +16,61 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/product (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/product')
       .expect(200)
-      .expect('Hello World!');
+      .expect((response) => {
+        expect(response.body).toBeInstanceOf(Array);
+        exampleProduct = response.body[0];
+      });
+  });
+
+  it('/product/{id} (GET)', () => {
+    return request(app.getHttpServer())
+      .get(`/product/${exampleProduct.id}`)
+      .expect(200)
+      .expect((response) => {
+        expect(response.body).toStrictEqual(exampleProduct);
+      });
+  });
+
+  it('/product (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/product')
+      .send({
+        name: 'e2e test product name',
+        price: '100',
+        image: 'test-url-image',
+      })
+      .expect(201);
+  });
+
+  it('/product/{id} (PUT)', () => {
+    const newCredentials = {
+      id: exampleProduct.id,
+      name: 'new e2e test product name',
+      price: '1200',
+      image: 'new test-url-image',
+    };
+
+    return request(app.getHttpServer())
+      .put(`/product/${exampleProduct.id}`)
+      .send(newCredentials)
+      .expect(200)
+      .expect((response) => {
+        expect(response.body).toStrictEqual(newCredentials);
+      });
+  });
+
+  it('/product/{id} (DELETE)', () => {
+    return request(app.getHttpServer())
+      .delete(`/product/${exampleProduct.id}`)
+      .expect(200);
+  });
+
+  afterAll((done) => {
+    app.close();
+    done();
   });
 });
